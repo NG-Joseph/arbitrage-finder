177d8f385852b68d81c4c5a2eac594afcb6b60f4
NG-Joseph <jcool4liv@gmail.com>
2021-09-19 16:10:24 +0100

    add utils

---
+++ utils.py
@@ -0,0 +1,23 @@
+
+'''
+This file contains utitlity functions for the project.
+
+'''
+
+
+def calculate_arbitrage (match: dict) -> str:
+    '''
+    Calculates the arbitrage value of a match.
+
+    Args:
+        match: A dictionary containing the match data such as {'home_team': 'Chelsea', 'away_team': 'Arsenal', 'home_odds': 1.5, 'away_odds': 1.5}
+
+    Returns:
+        The arbitrage value of the match as well as the profit of the arbitrage.
+    '''
+
+    # Get the match values
+ 
+
+
+    
