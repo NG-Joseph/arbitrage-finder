07eda7b46f728bdb53585cb5c7fafbd5ac3b66ee
NG-Joseph <jcool4liv@gmail.com>
2021-09-24 20:29:14 +0100

    complete scraping from sporty, next: betway

---
+++ betway.py
@@ -0,0 +1,4 @@
+'''
+Betway puts team names in line with odds so we need to separate them and store them in new variables via regex
+We also need to filter them by dates
+'''
--- sportybet.py
+++ sportybet.py
@@ -29,6 +29,8 @@ btts = []
 over_under=[]
 odds_events = []
 market_tabs = [] # dropdowns
+matches_dicts = []
+
 
 for entry in driver.get_log('browser'):
     print(entry)
@@ -37,13 +39,36 @@ league_wraps = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_locate
 
 for league_wrap in league_wraps:
 
-    # append all the market items (3way,double chance) to 'dropdowns' array
+    # append all the market items (3way,double chance) to market_tabs array and click on it
     market_tabs.append(league_wrap.find_elements_by_class_name("market-item"))
     market_tabs[league_wraps.index(league_wrap)][2].click()
+    """for match in league_wrap:
+        home_team = league_wrap[match].find_elements_by_class_name('home-team')
+        away_team = league_wrap[match].find_elements_by_class_name('away-team')
+        teams.append([home_team,away_team])"""
 
-odds = league_wraps[0].find_elements_by_class_name("m-outcome-odds")
-for item in odds:
-    print(item.text) # odds can be multiple per branch so print all
+match_rows = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'match-row')))
+
+for match_row in match_rows:
+    home_team = match_row.find_element_by_class_name('home-team').text
+    away_team = match_row.find_element_by_class_name('away-team').text
+    odds = match_row.find_elements_by_class_name('m-outcome-odds')
+    try:
+        home_odds = float(odds[0].text) # home comes first
+        away_odds = float(odds[1].text) # away comes second
+    except:
+        print('error was at', match_rows.index(match_row))
+
+
+    matches_dicts.append({'home_team': home_team, 'away_team': away_team, 'home_odds': home_odds, 'away_odds': away_odds})
+
+
+
+
+# next step: get home and away odds as well as team names in a dict and append it into a new array
+for i in matches_dicts:
+    print(i)
+
 
 
 
